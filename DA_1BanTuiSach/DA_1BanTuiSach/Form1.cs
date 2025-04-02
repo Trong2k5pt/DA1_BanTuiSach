61dd213b9c354e5be3de6b6cf3af3ba7df09d814
AEN25 <tienanh2k512@gmail.com>
2025-04-02 15:27:24 +0700

    Them Dang Nhap

--- DA_1BanTuiSach/DA_1BanTuiSach/Form1.cs
+++ DA_1BanTuiSach/DA_1BanTuiSach/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,47 @@ namespace DA_1BanTuiSach
         {
             InitializeComponent();
         }
-    }
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			SqlConnection conn = new SqlConnection(@"Data Source=ANH2005\SQLEXPRESS;Initial Catalog=QL02;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+			try
+			{
+				conn.Open();
+				string tk = textBox1.Text.Trim();
+				string mk = textBox2.Text.Trim(); // Chưa mã hóa, cần băm (hash) mật khẩu nếu CSDL đã lưu hash
+
+				string sql = "SELECT * FROM NhanVien WHERE taiKhoan = @tk AND matKhau = @mk";
+
+				using (SqlCommand cmd = new SqlCommand(sql, conn))
+				{
+					cmd.Parameters.AddWithValue("@tk", tk);
+					cmd.Parameters.AddWithValue("@mk", mk); // Nên sử dụng mật khẩu đã hash
+
+					using (SqlDataReader rdr = cmd.ExecuteReader())
+					{
+						if (rdr.Read())
+						{
+							MessageBox.Show("Đăng Nhập Thành Công");
+							Form2 form2 = new Form2();
+							form2.Show();
+						}
+						else
+						{
+							MessageBox.Show("Đăng Nhập Thất Bại");
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Lỗi Kết Nối: " + ex.Message);
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+	}
 }
