b4b956b71c8fa7819bfad17ea8feed5b54206f71
tainlaph48515 <tainlaph48515@gmail.com>
2025-04-03 22:58:47 +0700

    Thêm in hóa đơn

--- DA_1BanTuiSach/DA_1BanTuiSach/BLL/SanPhamChiTietBLL.cs
+++ DA_1BanTuiSach/DA_1BanTuiSach/BLL/SanPhamChiTietBLL.cs
@@ -1,22 +1,17 @@
-﻿using DA_1BanTuiSach.DTO.Model;
-using System;
+﻿using DA_1BanTuiSach.BLL;
+using DA_1BanTuiSach.DTO.Model;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DA_1BanTuiSach.BLL
+public class SanPhamChiTietBLL
 {
-    public class SanPhamChiTietBLL
+    public List<SanPhamChiTietView> GetAllSanPhamChiTiets()
     {
-        public List<SanPhamChiTietView> GetAllSanPhamChiTiets()
+        var list = new List<SanPhamChiTietView>();
+        using (SqlConnection conn = new SqlConnection(DbHelper.ConnectionString))
         {
-            var list = new List<SanPhamChiTietView>();
-            using (SqlConnection conn = new SqlConnection(DbHelper.ConnectionString))
-            {
-                conn.Open();
-                var cmd = new SqlCommand(@"SELECT  
+            conn.Open();
+            var cmd = new SqlCommand(@"SELECT  
                                 spct.maSanPhamChiTiet,
                                 spct.maSanPham,
                                 spct.tenSanPhamChiTiet,
@@ -30,33 +25,35 @@ namespace DA_1BanTuiSach.BLL
                             FROM SanPhamChiTiet spct
                             INNER JOIN SanPham sp ON spct.maSanPham = sp.maSanPham
                             INNER JOIN MauSac ms ON spct.maMauSac = ms.maMauSac", conn);
-                var reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    list.Add(new SanPhamChiTietView
-                    {
-                        MaSanPhamChiTiet = (int)reader["maSanPhamChiTiet"],
-                        TenSanPhamChiTiet = reader["tenSanPhamChiTiet"].ToString(),
-                        GiaSanPham = (decimal)reader["giaSanPham"],
-                        SoLuong = (int)reader["soLuong"],
-                        TrangThai = (bool)reader["trangThai"],
-                        Size = reader["size"].ToString(),
-                        ChatLieu = reader["chatLieu"].ToString(),
-                        KieuDang = reader["kieuDang"].ToString(),
-                        TenMau = reader["tenMau"].ToString()
-                    });
-                }
-            }
-            return list;
-        }
-
-        public SanPhamChiTietView GetSanPhamChiTietById(int id)
-        {
-            using (SqlConnection conn = new SqlConnection(DbHelper.ConnectionString))
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
             {
-                conn.Open();
-                var cmd = new SqlCommand(@"SELECT  
+                list.Add(new SanPhamChiTietView
+                {
+                    MaSanPhamChiTiet = (int)reader["maSanPhamChiTiet"],
+                    MaSanPham = (int)reader["maSanPham"],                         // ✅ Thêm dòng này
+                    TenSanPhamChiTiet = reader["tenSanPhamChiTiet"].ToString(),
+                    GiaSanPham = (decimal)reader["giaSanPham"],
+                    SoLuong = (int)reader["soLuong"],
+                    TrangThai = (bool)reader["trangThai"],
+                    Size = reader["size"].ToString(),
+                    ChatLieu = reader["chatLieu"].ToString(),
+                    KieuDang = reader["kieuDang"].ToString(),
+                    TenMau = reader["tenMau"].ToString()
+                });
+            }
+        }
+        return list;
+    }
+
+    public SanPhamChiTietView GetSanPhamChiTietById(int id)
+    {
+        using (SqlConnection conn = new SqlConnection(DbHelper.ConnectionString))
+        {
+            conn.Open();
+            var cmd = new SqlCommand(@"SELECT  
                                 spct.maSanPhamChiTiet,
+                                spct.maSanPham,
                                 spct.tenSanPhamChiTiet,
                                 spct.giaSanPham,
                                 spct.soLuong,
@@ -69,40 +66,37 @@ namespace DA_1BanTuiSach.BLL
                             INNER JOIN SanPham sp ON spct.maSanPham = sp.maSanPham
                             INNER JOIN MauSac ms ON spct.maMauSac = ms.maMauSac
                             WHERE spct.maSanPhamChiTiet = @id", conn);
-                cmd.Parameters.AddWithValue("@id", id);
-                var reader = cmd.ExecuteReader();
-                if (reader.Read())
-                {
-                    return new SanPhamChiTietView
-                    {
-                        MaSanPhamChiTiet = (int)reader["maSanPhamChiTiet"],
-                        TenSanPhamChiTiet = reader["tenSanPhamChiTiet"].ToString(),
-                        GiaSanPham = (decimal)reader["giaSanPham"],
-                        SoLuong = (int)reader["soLuong"],
-                        TrangThai = (bool)reader["trangThai"],
-                        Size = reader["size"].ToString(),
-                        ChatLieu = reader["chatLieu"].ToString(),
-                        KieuDang = reader["kieuDang"].ToString(),
-                        TenMau = reader["tenMau"].ToString()
-                    };
-                }
-                return null;
-            }
-        }
-
-        // Chú ý: Update vẫn dùng SanPhamChiTiet (chỉ update số lượng), không cần sửa.
-        public void Update(SanPhamChiTiet spct)
-        {
-            using (SqlConnection conn = new SqlConnection(DbHelper.ConnectionString))
+            cmd.Parameters.AddWithValue("@id", id);
+            var reader = cmd.ExecuteReader();
+            if (reader.Read())
             {
-                conn.Open();
-                var cmd = new SqlCommand("UPDATE SanPhamChiTiet SET soLuong = @sl WHERE maSanPhamChiTiet = @id", conn);
-                cmd.Parameters.AddWithValue("@sl", spct.SoLuong);
-                cmd.Parameters.AddWithValue("@id", spct.MaSanPhamChiTiet);
-                cmd.ExecuteNonQuery();
+                return new SanPhamChiTietView
+                {
+                    MaSanPhamChiTiet = (int)reader["maSanPhamChiTiet"],
+                    MaSanPham = (int)reader["maSanPham"],                         // ✅ Thêm dòng này
+                    TenSanPhamChiTiet = reader["tenSanPhamChiTiet"].ToString(),
+                    GiaSanPham = (decimal)reader["giaSanPham"],
+                    SoLuong = (int)reader["soLuong"],
+                    TrangThai = (bool)reader["trangThai"],
+                    Size = reader["size"].ToString(),
+                    ChatLieu = reader["chatLieu"].ToString(),
+                    KieuDang = reader["kieuDang"].ToString(),
+                    TenMau = reader["tenMau"].ToString()
+                };
             }
+            return null;
         }
     }
 
-
+    public void Update(SanPhamChiTiet spct)
+    {
+        using (SqlConnection conn = new SqlConnection(DbHelper.ConnectionString))
+        {
+            conn.Open();
+            var cmd = new SqlCommand("UPDATE SanPhamChiTiet SET soLuong = @sl WHERE maSanPhamChiTiet = @id", conn);
+            cmd.Parameters.AddWithValue("@sl", spct.SoLuong);
+            cmd.Parameters.AddWithValue("@id", spct.MaSanPhamChiTiet);
+            cmd.ExecuteNonQuery();
+        }
+    }
 }
---
+++ DA_1BanTuiSach/DA_1BanTuiSach/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DA_1BanTuiSach
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormBanHang()); // Đổi thành form chính của bạn nếu không phải FormBanHang
+        }
+    }
+}
