e5ccefca87b214b89604d13d8bad72b03a5ddb83
tainlaph48515 <tainlaph48515@gmail.com>
2025-04-03 20:50:56 +0700

    BanHang

---
+++ DA_1BanTuiSach/DA_1BanTuiSach/BLL/SanPhamBLL.cs
@@ -0,0 +1,38 @@
+﻿using DA_1BanTuiSach.DTO.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DA_1BanTuiSach.BLL
+{
+    public class SanPhamBLL
+    {
+        public List<SanPham> GetAllSanPhams()
+        {
+            var list = new List<SanPham>();
+            using (SqlConnection conn = new SqlConnection(DbHelper.ConnectionString))
+            {
+                conn.Open();
+                var cmd = new SqlCommand("SELECT * FROM SanPham", conn);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    list.Add(new SanPham
+                    {
+                        MaSanPham = (int)reader["maSanPham"],
+                        TenSanPham = reader["tenSanPham"].ToString(),
+                        Size = reader["size"].ToString(),
+                        ChatLieu = reader["chatLieu"].ToString(),
+                        KieuDang = reader["kieuDang"].ToString()
+                    });
+                }
+            }
+            return list;
+        }
+
+        public SanPham GetSanPhamById(int id) => GetAllSanPhams().FirstOrDefault(x => x.MaSanPham == id);
+    }
+}
---
+++ DA_1BanTuiSach/DA_1BanTuiSach/DTO/Model/HoaDon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DA_1BanTuiSach.DTO.Model
+{
+    public class HoaDon
+    {
+        public int MaHoaDon { get; set; }
+        public string MaHoaDonHienThi { get; set; } // Thêm vào để hiển thị
+        public string TenKhachHang { get; set; }
+        public string SoDienThoai { get; set; }
+        public DateTime NgayLapHoaDon { get; set; }
+        public string PhuongThucThanhToan { get; set; }
+        public decimal TongTienTamTinh { get; set; }
+        public decimal TongTien { get; set; }
+        public bool TrangThai { get; set; }
+        public int MaKhachHang { get; set; }
+        public int MaNhanVien { get; set; }
+    }
+
+}
