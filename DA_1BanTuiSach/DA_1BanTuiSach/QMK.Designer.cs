479f8c746a1f08201c1006c24f4e9297a460cc0d
AEN25 <tienanh2k512@gmail.com>
2025-04-08 21:58:02 +0700

    Add files via upload

---
+++ DA_1BanTuiSach/DA_1BanTuiSach/QMK.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace DA_1BanTuiSach
+{
+	partial class QMK
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(416, 127);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(267, 39);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Quên Mật Khẩu";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(434, 217);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(116, 18);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Email Nhân Viên";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(434, 280);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(146, 18);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Tài Khoản Nhân Viên";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(437, 238);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(188, 24);
+			this.textBox1.TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(437, 301);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(188, 24);
+			this.textBox2.TabIndex = 4;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(434, 365);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(191, 24);
+			this.textBox3.TabIndex = 5;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(434, 343);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 18);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "Mật Khẩu Mới";
+			// 
+			// linkLabel1
+			// 
+			this.linkLabel1.AutoSize = true;
+			this.linkLabel1.Location = new System.Drawing.Point(434, 392);
+			this.linkLabel1.Name = "linkLabel1";
+			this.linkLabel1.Size = new System.Drawing.Size(82, 18);
+			this.linkLabel1.TabIndex = 7;
+			this.linkLabel1.TabStop = true;
+			this.linkLabel1.Text = "Đăng Nhập";
+			this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+			// 
+			// button1
+			// 
+			this.button1.AutoSize = true;
+			this.button1.Location = new System.Drawing.Point(434, 443);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(83, 28);
+			this.button1.TabIndex = 8;
+			this.button1.Text = "Xác Nhận";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.AutoSize = true;
+			this.button2.Location = new System.Drawing.Point(550, 443);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 28);
+			this.button2.TabIndex = 9;
+			this.button2.Text = "Thoát";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// Form3
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1071, 593);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.linkLabel1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Name = "Form3";
+			this.Text = "Quên Mật Khẩu";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.LinkLabel linkLabel1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+	}
+}
